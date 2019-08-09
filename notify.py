74f2ff83bca4f3172335558776fc82103eab8e66
czh <120715979@qq.com>
2019-08-09 21:27:26 +0800

    a

---
+++ notify.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""一个更新通知"""
+
+import request
+
+def tx_manga():
+    # https://ac.qq.com/Comic/comicInfo/id/505430 海贼王地址
+    pass
+
+
+if __name__ == "__main__":
+    pass
